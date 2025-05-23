bf66dfd71b9752f29f04b263e0722d289540a0ac
byVelos <tobias.heiml@gmx.at>
2025-05-23 11:29:47 +0200

    Added a print in new function.

--- byVelos_poorhainz_android/dummy.java
+++ byVelos_poorhainz_android/dummy.java
@@ -1,10 +1,8 @@
 THIS IS NOT A REAL JAVA FILE
 
-<<<<<<< HEAD
 public void thisIsANewMethod(){
-    
-=======
+    System.out.printIn("Das ist eine Konflikt.");
+}
 public static void main(){
     System.out.printIn("Das ist eine Print Funktion.");
->>>>>>> 4dc67d5d5eab0947c6b5a28b71f17f6d1f6a0096
 }
