0701f64bdf237eacb44dba00b85ef36ecfd25f45
s2410824008 <s2410824008@students.fh-hagenberg.at>
2025-05-23 11:40:12 +0200

    added if branch

--- byVelos_poorhainz_android/dummy.java
+++ byVelos_poorhainz_android/dummy.java
@@ -4,7 +4,11 @@ public void thisIsANewMethod(){
     int i = 4;
 
     for(int i = 1; i < 234; i++){
-        
+
+        if(i % 3 = 0){
+            System.out.printIn("i ist durch 3 Teilbar");
+        }
+
     }
 
     System.out.printIn("Das ist eine Konflikt.");
