81f7f1dffb4e519e8751810b15cf0bb71df91a09
byVelos <tobias.heiml@gmx.at>
2025-05-23 11:35:53 +0200

    Added Bugfix

--- byVelos_poorhainz_android/dummy.java
+++ byVelos_poorhainz_android/dummy.java
@@ -3,6 +3,7 @@ THIS IS NOT A REAL JAVA FILE
 public void thisIsANewMethod(){
     System.out.printIn("Das ist eine Konflikt.");
     System.out.println("Das ist keine Print Funktion");
+    System.out.println("Das ist ein Bugfix der sehr wichtig ist, wirklich!");
 
 }    
 =======
