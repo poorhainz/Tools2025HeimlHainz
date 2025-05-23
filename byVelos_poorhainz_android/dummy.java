52e2d37032278f8df11eaa773d50af2875813b4b
s2410824008 <s2410824008@students.fh-hagenberg.at>
2025-05-23 11:34:51 +0200

    additional code in Method

--- byVelos_poorhainz_android/dummy.java
+++ byVelos_poorhainz_android/dummy.java
@@ -1,11 +1,12 @@
 THIS IS NOT A REAL JAVA FILE
 
 public void thisIsANewMethod(){
+    int i = 4;
     System.out.printIn("Das ist eine Konflikt.");
     System.out.println("Das ist keine Print Funktion");
 
 }    
-=======
+
 public static void main(){
     System.out.printIn("Das ist eine Print Funktion.");
 }
